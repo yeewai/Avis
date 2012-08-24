4d9dc5dcd736e901bb4707cf3abd288b2b5d8ea4
Yee <yeekiwai@gmail.com>
2012-08-23 23:03:22 -0700

    Task icons

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -2,13 +2,13 @@ module TasksHelper
   def task_type_image(kind)
     case kind
       when Task::TASK_TASK
-        "status-red.png"
+        "task-task.png"
       when Task::TASK_CHORE
-        "status-amber.png"
+        "task-chore.png"
       when Task::TASK_ERROR
-        "status-green.png"
+        "task-error.png"
       when Task::TASK_SUGGESTION
-        "status-teal.png"
+        "task-idea.png"
     end
   end
   
@@ -27,13 +27,17 @@ module TasksHelper
   
   def task_status(status)
     case status
-      when Task::TASK_TASK
+      when Task::TASK_ORPHANED
         "status-red.png"
-      when Task::TASK_CHORE
+      when Task::TASK_REJECTED
+        "status-red.png"
+      when Task::TASK_STARTED
         "status-amber.png"
-      when Task::TASK_ERROR
+      when Task::TASK_ASSIGNED
+        "status-amber.png"
+      when Task::TASK_FINISHED
         "status-green.png"
-      when Task::TASK_SUGGESTION
+      when Task::TASK_ACCEPTED
         "status-teal.png"
     end
   end
