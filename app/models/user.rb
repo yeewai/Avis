32093798cf4c83e3be85f7346a8d6a30db275a6c
Yee <yeekiwai@gmail.com>
2012-09-17 11:29:39 -0500

    s3 for user avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,7 +33,19 @@ class User < ActiveRecord::Base
   
   
   #avatar stuffs
-  has_attached_file :avatar
+  # s3 Credentials
+  options = if Rails.env.production? 
+    {:storage => :s3,
+     :bucket => ENV['S3_BUCKET_NAME'],
+     :s3_credentials => {
+       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']}
+     }
+  else
+     {}  
+  end
+  
+  has_attached_file :avatar, options
   validates_attachment_size :avatar, :less_than => 2.megabytes
   validates_attachment_content_type :avatar, :content_type=>['image/jpeg', 'image/png', 'image/gif']
   
